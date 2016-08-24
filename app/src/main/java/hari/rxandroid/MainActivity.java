1250967465a3b51a396fa700abbb4df9a69d76b4
Hari Prasad <dnshariprasad@gmail.com>
2016-08-24 19:07:29 +0530

    RxAndroid+Retrofit+Lambda Expression

--- app/src/main/java/hari/rxandroid/MainActivity.java
+++ app/src/main/java/hari/rxandroid/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -33,21 +32,8 @@ public class MainActivity extends AppCompatActivity {
         ipApiService.getLocationInfo()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<String>() {
-                    @Override
-                    public void onCompleted() {
-                        Toast.makeText(MainActivity.this, "onCompleted", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onNext(String s) {
-                        tv_output.setText(s);
-                    }
-                });
+                .subscribe(s -> tv_output.setText(s),
+                        throwable -> Toast.makeText(MainActivity.this, throwable.getLocalizedMessage(), Toast.LENGTH_SHORT).show(),
+                        () -> Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_SHORT).show());
     }
 }
