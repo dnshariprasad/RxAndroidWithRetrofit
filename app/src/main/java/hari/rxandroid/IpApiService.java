357b1cb2105e15b823d6f4e96fb478eb8bc61112
Hari Prasad <dnshariprasad@gmail.com>
2016-08-24 18:44:42 +0530

    first commit

--- app/src/main/java/hari/rxandroid/IpApiService.java
+++ app/src/main/java/hari/rxandroid/IpApiService.java
@@ -2,11 +2,12 @@ package hari.rxandroid;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import rx.Observable;
 
 /**
  * Created by Hari Prasad on 8/24/16.
  */
 public interface IpApiService {
     @GET("/json")
-    Call<String> getLocationInfor();
+    Observable<String> getLocationInfor();
 }
--- app/src/main/java/hari/rxandroid/MainActivity.java
+++ app/src/main/java/hari/rxandroid/MainActivity.java
@@ -1,20 +1,14 @@
 package hari.rxandroid;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func0;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,9 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_output = (TextView) findViewById(R.id.tv_output);
-        getIpApiObservable()
-                .subscribeOn(Schedulers.io())
+
+        Retrofit ipApiRetrofit = new Retrofit.Builder()
+                .baseUrl("http://ip-api.com")
+                .addConverterFactory(StringConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        IpApiService ipApiService = ipApiRetrofit.create(IpApiService.class);
+
+        ipApiService.getLocationInfor()
                 .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
                 .subscribe(new Subscriber<String>() {
                     @Override
                     public void onCompleted() {
@@ -44,32 +46,5 @@ public class MainActivity extends AppCompatActivity {
                         tv_output.setText(s);
                     }
                 });
-    }
-
-    @Nullable
-    private String getIpApi() throws IOException {
-        Retrofit ipApiRetrofit = new Retrofit.Builder()
-                .baseUrl("http://ip-api.com")
-                .addConverterFactory(StringConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        IpApiService ipApiService = ipApiRetrofit.create(IpApiService.class);
-        Response<String> response = ipApiService.getLocationInfor().execute();
-        if (response.isSuccessful()) return response.body();
-        else return null;
-    }
-
-    private Observable<String> getIpApiObservable() {
-        return Observable.defer(new Func0<Observable<String>>() {
-            @Override
-            public Observable<String> call() {
-                try {
-                    return rx.Observable.just(getIpApi());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-        });
     }
 }
